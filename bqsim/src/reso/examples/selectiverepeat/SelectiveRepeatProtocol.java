09bd0090609167a9ef192e4a2def5f92f7ad4424
GuillaumeProot <guillaume.proot@student.umons.ac.be>
2019-05-07 10:01:09 +0200

    suite protocol

--- bqsim/src/reso/examples/selectiverepeat/SelectiveRepeatProtocol.java
+++ bqsim/src/reso/examples/selectiverepeat/SelectiveRepeatProtocol.java
@@ -1,11 +1,11 @@
 package reso.examples.selectiverepeat;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
-import reso.ip.Datagram;
-import reso.ip.IPHost;
-import reso.ip.IPInterfaceAdapter;
-import reso.ip.IPInterfaceListener;
+import reso.common.AbstractTimer;
+import reso.ip.*;
+import reso.scheduler.AbstractScheduler;
 
 public class SelectiveRepeatProtocol implements IPInterfaceListener {
     private ArrayList queue;
@@ -20,6 +20,9 @@ public class SelectiveRepeatProtocol implements IPInterfaceListener {
     private int N;
     private float lostAck, lostPacket;
     private final IPHost host;
+    private MyTimer timer;
+    private double alpha, beta, rttEchantillon, rttEstime, rto,rtt;
+    private AbstractScheduler scheduler;
 
     public SelectiveRepeatProtocol(IPHost host, float lostPacket, float lostAck) {
         this.numSeq = 0;
@@ -28,6 +31,10 @@ public class SelectiveRepeatProtocol implements IPInterfaceListener {
         this.host = host;
         this.lostAck = lostAck;
         this.lostPacket = lostPacket;
+        scheduler=host.getNetwork().getScheduler();
+        alpha = 0.125;
+        beta = 0.25;
+        rttEstime = 1;
     }
 
     @Override
@@ -35,17 +42,46 @@ public class SelectiveRepeatProtocol implements IPInterfaceListener {
         
     }
 
-    public void send(IpAddress src, String data) throws Exeption {
+    public void send(IPAddress src, String data) throws Exception {
+
+
         if(!isInit && !initMessageSend){
-            SelectiveRepeatMessage message = new SelectiveRepeatMessage(INIT_REQUEST, Integer.MIN_VALUE);
+            SelectiveRepeatMessage message = new SelectiveRepeatMessage( Integer.MIN_VALUE,INIT_REQUEST);
             host.getIPLayer().send(IPAddress.ANY, src, IP_PROTO_SELECTIVEREPEAT, message);
         }
-        SelectiveRepeatMessage message = new SelectiveRepeatMessage(data, numSeq);
+        SelectiveRepeatMessage message = new SelectiveRepeatMessage( numSeq,data);
         SelectiveRepeatPacket packet = new SelectiveRepeatPacket(message, src, -1);
         window.offer(packet);
         it = window.iterator();
         numSeq++;
         if (isInit)
             sendNext();
+    }
+
+    public void checkTimer(Queue<SelectiveRepeatPacket> window, boolean stop){
+        if(window.size() == 0 || stop){
+            return
+        }
+        else{
+            for(SelectiveRepeatPacket packet: window){
+                if(packet.timer
+            }
+        }
+    }
+
+    private class MyTimer extends AbstractTimer {
+        public MyTimer(AbstractScheduler scheduler, double interval) {
+            super(scheduler, interval, false);
+        }
+
+        protected void run() throws Exception {
+            System.out.println(scheduler.getCurrentTime()+": ---- TIMEOUT");
+            N=controller.isALoss(true);// change the size of the window because there is a timeout
+            NChanged(true);
+            next_seq_num = sendBase;
+            it=window.iterator();
+            sendNext();
+
+        }
     }
 }
